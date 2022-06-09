5f58dd9b2b5cd5cac9aab3e5ad11418cda0e2801
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-06-09 19:50:33 +0430

    playing with ruby

--- decode.rb
+++ decode.rb
@@ -1,3 +1,13 @@
-puts "Hello puts "
-print "Hello Print\n"
-p "Hello p"
+first_name = "Sediq"
+last_name = "badakhsh"
+full_name = "My name is #{first_name} and my last name is #{last_name}"
+puts full_name
+
+def add(a,b)
+    sum = a+b
+    puts "the sum of #{a} + #{b}: #{sum}"
+end
+
+add(1,3)
+add(3,4)
+add(5,2)
