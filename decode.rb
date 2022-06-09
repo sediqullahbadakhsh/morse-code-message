6065867b6942a5ff9ca29318edd2c129fde9a1b7
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-06-09 21:44:30 +0430

    decode the Morse code message

--- decode.rb
+++ decode.rb
@@ -1,13 +1,41 @@
-first_name = "Sediq"
-last_name = "badakhsh"
-full_name = "My name is #{first_name} and my last name is #{last_name}"
-puts full_name
+def decode_char(morse_char)
+  morse_code_hash = {
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D',
+    '.' => 'E', '..-.' => 'F', '--.' => 'G', '....' => 'H',
+    '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
+    '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P',
+    '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+    '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X',
+    '-.--' => 'Y', '--..' => 'Z', '.----' => '1', '..---' => '2',
+    '...--' => '3', '....-' => '4', '.....' => '5', '-....' => '6',
+    '--...' => '7', '---..' => '8', '----.' => '9', '-----' => '0'
+  }
+  morse_code_hash[morse_char]
+end
+# Test Single Character
+puts decode_char(".-")        # is A
+puts decode_char("-----")     # is 0
+puts decode_char("--.-")      # is Q
 
-def add(a,b)
-    sum = a+b
-    puts "the sum of #{a} + #{b}: #{sum}"
+def decode_word(morse_word)
+  characters = morse_word.split
+  decoded_character = ''
+  characters.each { |char| decoded_character = "#{decoded_character}#{decode_char(char)}" }
+  decoded_character
 end
 
-add(1,3)
-add(3,4)
-add(5,2)
+# Test Words
+puts decode_word("-- -.--") # is MY
+
+def decode(morse_sentence)
+  words = morse_sentence.split('   ')
+  decoded_words = []
+  words.each { |word| decoded_words.push(decode_word(word)) }
+  decoded_words.join(' ')
+end
+
+# Test Sentence
+puts decode("-- -.--   -. .- -- .") # is MY NAME
+
+# Morse Code Message
+puts decode(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
